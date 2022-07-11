5936f102fa9b29f37a9307c9b1a91b3c2f78ca23
Andrew Gary <Andrew.gary91@gmail.com>
2022-07-11 15:07:29 -0500

    commiting before integrating authorization

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,14 +5,9 @@ import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon, ShoppingCartIcon, SearchIcon } from '@heroicons/react/outline'
 import { useAuth0 } from '@auth0/auth0-react';
 
-const defaultUser = {
-  name: 'Tom Cook',
-  email: 'tom@example.com',
-  imageUrl:
-    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-}
+
 const navigation = [
-  { name: 'Home', href: '#', current: true },
+  { name: 'Home', href: '/', current: false },
   { name: 'Shop', href: '/products', current: false },
   { name: 'NewWork', href: '#', current: false },
   { name: 'Contact', href: '#', current: false },
